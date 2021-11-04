e6a62fb8b85dbe9e9dd74d3fbcc88d525f07dc9d
Blake Robert Mills <82824419+BlakeRMills@users.noreply.github.com>
2021-11-03 23:52:34 -0400

    Update Day 3 (Polygons).R

--- Day 3 (Polygon)/Day 3 (Polygons).R
+++ Day 3 (Polygon)/Day 3 (Polygons).R
@@ -13,7 +13,6 @@ library(sysfonts)
 nyc <- geojson_sf("~/Desktop/Parks/Parks Nyc.geojson")
 paris <- geojson_sf("~/Desktop/Parks/Parks Paris.geojson")
 amsterdam <- geojson_sf("~/Desktop/Parks/Parks Amsterdam.geojson")[1:120, ] #Last two parks are not closed polygons
-london <- geojson_sf("~/Desktop/Parks/Parks London.geojson")
 chicago <- geojson_sf("~/Desktop/Parks/Parks Chicago.geojson")
 milan <- geojson_sf("~/Desktop/Parks/Parks Milan.geojson")
 toronto <- readOGR(dsn = "~/Desktop/Parks/Parks Toronto") %>% tidy()
@@ -45,29 +44,24 @@ laP <- ggplot() +
   xlim(c(-118.7, -118.12)) +
   theme_void()
 
-londonP <- ggplot() +
-  geom_sf(data=london, aes(geometry=geometry), fill=colors[4], color=colors[4], size=1.25) +
-  theme_void()+
-  theme(plot.margin = margin(2, 0, 0, 0, unit="cm"))
-
 milanP <- ggplot() +
-  geom_sf(data=milan, aes(geometry=geometry), fill=colors[5], color=colors[5], size=1.25) +
+  geom_sf(data=milan, aes(geometry=geometry), fill=colors[4], color=colors[4], size=1.25) +
   theme_void()+
   theme(plot.margin = margin(2, 0, 0, 0, unit="cm"))
 
 nycP <- ggplot() +
-  geom_sf(data=nyc, aes(geometry=geometry), fill=colors[6], color=colors[6], size=1) +
+  geom_sf(data=nyc, aes(geometry=geometry), fill=colors[5], color=colors[5], size=1) +
   theme_void() +
   theme(plot.margin = margin(2, 0, 0, 0, unit="cm"))
 
 parisP <- ggplot() +
-  geom_sf(data=paris, aes(geometry=geometry), fill=colors[7], color=colors[7], size=1) +
+  geom_sf(data=paris, aes(geometry=geometry), fill=colors[6], color=colors[6], size=1) +
   ylim(c(48.8, 48.92)) +
   theme_void() +
   theme(plot.margin = margin(-1, 0, 0, 0, unit="cm"))
 
 perthP <- ggplot() +
-  geom_sf(data=perth, aes(geometry=geometry), fill=colors[10], color=colors[10], size=1) +
+  geom_sf(data=perth, aes(geometry=geometry), fill=colors[7], color=colors[7], size=1) +
   theme_void()
 
 singaporeP <- ggplot() +
@@ -86,8 +80,8 @@ torontoP <- ggplot() +
 
 # Main Grid
 PlotGrid <- plot_grid(amsterdamP, chicagoP, laP,
-          londonP, milanP, nycP,
-          parisP, singaporeP, torontoP, nrow=3, ncol=3) +
+          milanP, nycP,parisP,
+          perthP, singaporeP, torontoP, nrow=3, ncol=3) +
   theme(plot.background = element_rect(fill="#fdf9f5", color="#fdf9f5"),
         plot.margin = margin(8,0,0,0, unit="cm"))
 
@@ -103,10 +97,10 @@ ggdraw(PlotGrid) +
   draw_label(label="AMSTERDAM", x=0.15, y=0.883, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[1]) +
   draw_label(label="CHICAGO", x=0.5, y=0.883, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[2]) +  
   draw_label(label="LOS ANGELES", x=0.85, y=0.883, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[3]) +
-  draw_label(label="LONDON", x=0.15, y=0.568, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[4]) +
-  draw_label(label="MILAN", x=0.5, y=0.568, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[5]) +
-  draw_label(label="NEW YORK CITY", x=0.85, y=0.568, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[6]) +
-  draw_label(label="PARIS", x=0.15, y=0.268, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[7]) +
+  draw_label(label="MILAN", x=0.15, y=0.568, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[4]) +
+  draw_label(label="NEW YORK CITY", x=0.5, y=0.568, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[5]) +
+  draw_label(label="PARIS", x=0.85, y=0.568, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[6]) +
+  draw_label(label="PERTH", x=0.15, y=0.268, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[7]) +
   draw_label(label="SINGAPORE", x=0.5, y=0.268, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[8]) +
   draw_label(label="TORONTO", x=0.85, y=0.268, size=fontsize, fontfamily = font1, fontface = "bold", color = colors[9]) 
 
