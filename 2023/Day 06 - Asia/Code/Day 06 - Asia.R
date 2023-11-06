f14368f6b32aff97759fc755ce7b7589a027b991
Blake Robert Mills <82824419+BlakeRMills@users.noreply.github.com>
2023-11-06 08:30:05 -0500

    Day 06 - Asia

---
+++ 2023/Day 06 - Asia/Code/Day 06 - Asia.R
@@ -0,0 +1,16 @@
+# Libraries
+library(cowplot)
+library(sf)
+library(showtext)
+library(sysfonts)
+library(tidyverse)
+
+# Aes
+showtext_auto()
+font <- "Advent Pro"
+font_add_google(font)
+
+# Data
+
+
+
