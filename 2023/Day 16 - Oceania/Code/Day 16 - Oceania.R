e8b02ff2457c0f39c9b66854be8b6410bd088730
Blake Robert Mills <82824419+BlakeRMills@users.noreply.github.com>
2023-11-18 10:47:18 -0500

    Day 16 - Oceania

---
+++ 2023/Day 16 - Oceania/Code/Day 16 - Oceania.R
@@ -0,0 +1,15 @@
+# Libraries
+library(cowplot)
+library(MoMAColors)
+library(tidyverse)
+library(sf)
+library(showtext)
+library(sysfonts)
+
+# Aes
+sf_use_s2(FALSE)
+showtext_auto()
+font <- "Advent Pro"
+font_add_google(font)
+
+# Data
