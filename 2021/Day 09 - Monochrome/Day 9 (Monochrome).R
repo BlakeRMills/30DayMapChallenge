c0d3353fa53d240e2e69eaf0521a1370b6a771e1
Blake Robert Mills <82824419+BlakeRMills@users.noreply.github.com>
2023-11-19 11:41:41 -0500

    File Renaming

---
+++ 2021/Day 09 - Monochrome/Day 9 (Monochrome).R
@@ -0,0 +1,326 @@
+# Libraries
+library(osmdata)
+library(tidyverse)
+library(sf)
+library(cowplot)
+library(showtext)
+library(sysfonts)
+
+# Aes
+showtext_auto()
+Colors <- c("#0d0649", "#243e8a", "#3b5fa7", "#6ea3ca", "#a7cae3")
+font_add_google("Quicksand")
+font_add_google("Fira Sans")
+font1 <- "Fira Sans"
+font2 <- "Quicksand"
+
+
+# Border Definiton
+## London
+LondonBorder <- opq(bbox=c(-0.55, 51.2911, 0.25, 51.744)) %>%
+  add_osm_feature(key="admin_level", value="6") %>%
+  osmdata_sf() %>%
+  .$osm_multipolygons %>%
+  select(osm_id, name, geometry) %>%
+  filter(name %in% c("London", "City of London")) %>%
+  st_union()
+
+## Milan
+MilanBorder <- opq(bbox="Milan, Italy") %>%
+  add_osm_feature(key="admin_level", value="8") %>%
+  osmdata_sf() %>%
+  .$osm_multipolygons %>%
+  select(osm_id, name, geometry) %>%
+  filter(name =="Comune di Milano") 
+
+## New York
+NycBorder <- opq(bbox="New York") %>%
+  add_osm_feature(key="admin_level", value="5") %>%
+  osmdata_sf() %>%
+  .$osm_multipolygons %>%
+  select(osm_id, name, geometry) 
+
+## Paris
+ParisBorder <- opq(bbox="Paris, France") %>%
+  add_osm_feature(key="admin_level", value="8") %>%
+  osmdata_sf() %>%
+  .$osm_multipolygons %>%
+  select(osm_id, name, geometry) %>%
+  filter(name=="Paris")
+
+## Sydney
+SydneyBorder <- opq(bbox="Sydney, Australia") %>%
+  add_osm_feature(key="admin_level", value="7") %>%
+  osmdata_sf() %>%
+  .$osm_multipolygons %>%
+  select(osm_id, name, geometry)
+
+
+## Mexico City
+MexicoCityBorder <- opq(bbox="Mexico City, Mexico") %>%
+  add_osm_feature(key="admin_level", value="4") %>%
+  osmdata_sf() %>%
+  .$osm_multipolygons %>%
+  select(osm_id, name, geometry) %>%
+  filter(name=="Ciudad de México")
+
+## Berlin
+BerlinBorder <- opq(bbox="Berlin, Germany") %>%
+  add_osm_feature(key="admin_level", value="4") %>%
+  osmdata_sf() %>%
+  .$osm_multipolygons %>%
+  select(osm_id, name, geometry) %>%
+  filter(name=="Berlin")
+
+
+# Pedestrians
+PedFun <- function(bbox, border){
+  Pedestrian <- opq(bbox=bbox) %>%
+    add_osm_features(features=c("\"highway\"=\"pedestrian\"",
+                                "\"highway\"=\"footway\"",
+                                "\"highway\"=\"path\"",
+                                "\"highway\"=\"living_street\"",
+                                "\"highway\"=\"residential\"",
+                                "\"footway\"=\"sidewalk\"",
+                                "\"footway\"=\"crossing\"",
+                                "\"route\"=\"foot\""))  %>%
+    osmdata_sf() %>%
+    .$osm_lines %>%
+    select(osm_id, name, geometry) %>%
+    filter(st_is_valid(.)==TRUE)
+  Pedestrian <- st_intersection(Pedestrian, border)
+  return(Pedestrian)
+}
+
+BerlinPed <- PedFun(c(13.099, 52.6674, 13.7892, 52.337), BerlinBorder)
+LondonPed <- PedFun(c(-0.55, 51.2911, 0.25, 51.744), LondonBorder)
+#MexCityPed <- PedFun("Mexico City, Mexico", MexicoCityBorder)
+MilanPed <- PedFun("Milan, Italy", MilanBorder)
+NycPed <- PedFun("New York City", NycBorder)
+#SydneyPed <- PedFun("Sydney, Australia", SydneyBorder)
+ParisPed <- PedFun("Paris, France", ParisBorder)
+
+
+
+# Subways
+SubFun <- function(bbox, border){
+  Subway <- opq(bbox=bbox) %>%
+    add_osm_features(features=c("\"railway\"=\"light_rail\"",
+                                "\"railway\"=\"subway\"",
+                                "\"route\"=\"subway\"",
+                                "\"railway\"=\"rail\"",
+                                "\"railway\"=\"tram\"",
+                                "\"route\"=\"railway\"",
+                                "\"route\"=\"light_rail\""))  %>%
+    osmdata_sf() %>%
+    .$osm_lines %>%
+    select(osm_id, name, geometry) %>%
+    filter(st_is_valid(.)==TRUE)
+  Subway <- st_intersection(Subway, border)
+  return(Subway)
+}
+
+BerlinSub <- SubFun(c(13.099, 52.6674, 13.7892, 52.337), BerlinBorder)
+LondonSub <- SubFun(c(-0.55, 51.2911, 0.25, 51.744), LondonBorder)
+#MexCitySub <- SubFun("Mexico City, Mexico", MexicoCityBorder)
+MilanSub <- SubFun("Milan, Italy", MilanBorder)
+NycSub <- SubFun("New York City", NycBorder)
+#SydneySub <- SubFun("Sydney, Australia", SydneyBorder)
+ParisSub <- SubFun("Paris, France", ParisBorder)
+
+
+
+
+# Greenspace 
+GreenFun <- function(bbox, border){
+  Greenspace1 <- opq(bbox=bbox) %>%
+    add_osm_features(features=c("\"landuse\"=\"village_green\"",
+                                "\"landuse\"=\"recreation_ground\"",
+                                "\"landuse\"=\"forest\"",
+                                "\"landuse\"=\"allotments\"",
+                                "\"landuse\"=\"meadow\"",
+                                "\"landuse\"=\"grass\""))  %>%
+    osmdata_sf() 
+  Greenspace1 <- rbind(Greenspace1 %>% .$osm_polygons %>% select(osm_id, name, geometry),
+                       Greenspace1 %>% .$osm_multipolygons %>% select(osm_id, name, geometry))
+  
+  Greenspace2 <- opq(bbox=bbox) %>%
+    add_osm_features(features=c("\"leisure\"=\"garden\"",
+                                "\"leisure\"=\"park\"",
+                                "\"natural\"=\"wood\"",
+                                "\"natural\"=\"grassland\""))  %>%
+    osmdata_sf() 
+  
+  Greenspace2 <- rbind(Greenspace2 %>% .$osm_polygons %>% select(osm_id, name, geometry),
+                       Greenspace2 %>% .$osm_multipolygons %>% select(osm_id, name, geometry))
+  
+  Greenspace <- rbind(Greenspace1, Greenspace2 ) %>% filter(st_is_valid(.)==TRUE)
+  Greenspace <- st_intersection(Greenspace, border)
+  
+  return(Greenspace)
+}
+
+BerlinGreen <- GreenFun(c(13.099, 52.6674, 13.7892, 52.337), BerlinBorder)
+LondonGreen <- GreenFun(c(-0.55, 51.2911, 0.25, 51.744), LondonBorder)
+#MexCityGreen <- GreenFun("Mexico City, Mexico", MexicoCityBorder)
+MilanGreen <- GreenFun("Milan, Italy", MilanBorder)
+NycGreen <- GreenFun("New York City", NycBorder)
+#SydneyGreen <- GreenFun("Sydney, Australia", SydneyBorder)
+ParisGreen <- GreenFun("Paris, France", ParisBorder)
+
+
+
+# Bikes
+BikeFun <- function(bbox, border){
+  Bikes <- opq(bbox=bbox) %>%
+    add_osm_features(features=c("\"highway\"=\"cycleway\"",
+                                "\"cycleway\"=\"lane\"",
+                                "\"cycleway\"=\"track\"",
+                                "\"route\"=\"bicycle\""))  %>%
+    osmdata_sf() %>%
+    .$osm_lines %>%
+    select(osm_id, name, geometry) %>%
+    filter(st_is_valid(.)==TRUE)
+  Bikes <- st_intersection(Bikes, border)
+  return(Bikes)
+}
+
+BerlinBike <- BikeFun(c(13.099, 52.6674, 13.7892, 52.337), BerlinBorder)
+LondonBike <- BikeFun(c(-0.55, 51.2911, 0.25, 51.744), LondonBorder)
+#MexCityBike <- BikeFun("Mexico City, Mexico", MexicoCityBorder)
+MilanBike <- BikeFun("Milan, Italy", MilanBorder)
+NycBike <- BikeFun("New York City", NycBorder)
+#SydneyBike <- BikeFun("Sydney, Australia", SydneyBorder)
+ParisBike <- BikeFun("Paris, France", ParisBorder)
+
+
+# Water 
+waterFun <- function(bbox, border){
+  Water1 <- opq(bbox=bbox) %>%
+    add_osm_features(features=c("\"water\"=\"pond\"",
+                                "\"water\"=\"lake\"",
+                                "\"water\"=\"reservoir\"",
+                                "\"water\"=\"river\"",
+                                "\"water\"=\"canal\"",
+                                "\"water\"=\"lock\""))  %>%
+    osmdata_sf() 
+  Water1 <- rbind(Water1 %>% .$osm_polygons %>% select(osm_id, name, geometry),
+                  Water1 %>% .$osm_multipolygons %>% select(osm_id, name, geometry))
+  
+  Water2 <- opq(bbox=bbox) %>%
+    add_osm_features(features=c("\"waterway\"=\"river\"",
+                                "\"waterway\"=\"stream\"",
+                                "\"waterway\"=\"canal\"",
+                                "\"landuse\"=\"reservoir\"",
+                                "\"natural\"=\"water\"",
+                                "\"natural\"=\"strait\"",
+                                "\"natural\"=\"bay\""))  %>%
+    osmdata_sf() 
+  
+  Water2 <- rbind(Water2 %>% .$osm_polygons %>% select(osm_id, name, geometry),
+                  Water2 %>% .$osm_multipolygons %>% select(osm_id, name, geometry))
+  
+  Water <- rbind(Water2, Water2) %>% filter(st_is_valid(.)==TRUE)
+  Water <- st_intersection(Water, border)
+  
+  return(Water)
+}
+
+BerlinWater <- waterFun(c(13.099, 52.6674, 13.7892, 52.337), BerlinBorder)
+LondonWater <- waterFun(c(-0.55, 51.2911, 0.25, 51.744), LondonBorder)
+#MexCityBike <- waterFun("Mexico City, Mexico", MexicoCityBorder)
+MilanWater <- waterFun("Milan, Italy", MilanBorder)
+NycWater <- waterFun("New York City", NycBorder)
+#SydneyBike <- waterFun("Sydney, Australia", SydneyBorder)
+ParisWater <- waterFun("Paris, France", ParisBorder)
+
+# Plots
+
+plotter <- function(Frame, color, size, border){
+  ggplot() +
+    geom_sf(data=border, fill="#fbefe4", size=0.4, color="#fbefe4", alpha=0.8) +
+    geom_sf(data=Frame, fill=color, color=color, size=size) +
+    theme_void() +
+    theme(plot.margin = margin(0,2,-0.5,2, "cm"))
+}
+
+
+## Bikes
+BikeGrid <- plot_grid(plotter(BerlinBike, Colors[1], 0.8, BerlinBorder),
+                      plotter(LondonBike, Colors[1], 0.8, LondonBorder),
+                      plotter(MilanBike, Colors[1], 0.8, MilanBorder),
+                      plotter(NycBike, Colors[1], 0.8, NycBorder),
+                      plotter(ParisBike, Colors[1], 0.8, ParisBorder),
+                      nrow=1) +
+  theme(plot.background = element_rect(fill="#fdf9f5", color="#fdf9f5"))
+
+## Green Space Grid
+GreenGrid <- plot_grid(plotter(BerlinGreen, Colors[2], 0.8, BerlinBorder),
+                       plotter(LondonGreen, Colors[2], 0.8, LondonBorder),
+                       plotter(MilanGreen, Colors[2], 0.8, MilanBorder),
+                       plotter(NycGreen, Colors[2], 0.8, NycBorder),
+                       plotter(ParisGreen, Colors[2], 0.8, ParisBorder),
+                       nrow=1) +
+  theme(plot.background = element_rect(fill="#fdf9f5", color="#fdf9f5"))
+
+
+
+## Pedestrian
+PedGrid <- plot_grid(plotter(BerlinPed, Colors[3], 0.12, BerlinBorder),
+                     plotter(LondonPed, Colors[3], 0.12, LondonBorder),
+                     plotter(MilanPed, Colors[3], 0.12, MilanBorder),
+                     plotter(NycPed, Colors[3], 0.12, NycBorder),
+                     plotter(ParisPed, Colors[3], 0.12, ParisBorder),
+                     nrow=1) +
+  theme(plot.background = element_rect(fill="#fdf9f5", color="#fdf9f5"))
+
+
+## Subways
+SubwayGrid <- plot_grid(plotter(BerlinSub, Colors[4], 0.8, BerlinBorder),
+                        plotter(LondonSub, Colors[4], 0.8, LondonBorder),
+                        plotter(MilanSub, Colors[4], 0.8, MilanBorder),
+                        plotter(NycSub, Colors[4], 0.8, NycBorder),
+                        plotter(ParisSub, Colors[4], 0.8, ParisBorder),
+                        nrow=1) +
+  theme(plot.background = element_rect(fill="#fdf9f5", color="#fdf9f5"))
+
+
+
+
+## Water
+WaterGrid <- plot_grid(plotter(BerlinWater, Colors[5], 0.8, BerlinBorder),
+                       plotter(LondonWater, Colors[5], 0.8, LondonBorder),
+                       plotter(MilanWater, Colors[5], 0.8, MilanBorder),
+                       plotter(NycWater, Colors[5], 0.8, NycBorder),
+                       plotter(ParisWater, Colors[5], 0.8, ParisBorder),
+                       nrow=1) +
+  theme(plot.background = element_rect(fill="#fdf9f5", color="#fdf9f5"))
+
+
+
+MainGrid <- plot_grid(BikeGrid, GreenGrid, PedGrid, SubwayGrid, WaterGrid, ncol=1) +
+  theme(plot.background = element_rect(fill="#fdf9f5", color="#fdf9f5"),
+        panel.background = element_rect(fill="#fdf9f5", color="#fdf9f5"),
+        plot.margin = margin(8, 0, 3, 6, unit="cm"))
+
+ggdraw(MainGrid) +
+  draw_label(x=0.5, y=0.96, label = "5 Maps of 5 Cities", color="#060633", size=475, fontfamily=font1, fontface="bold") +
+  draw_line(x=c(0, 0.24), y=c(0.96, 0.96), color="#060633", size=5.5) +
+  draw_line(x=c(0.76, 1), y=c(0.96, 0.96), color="#060633", size=5.5) +
+  #draw_label(x=0.5, y=0.93, label = "Different ways of mapping global cities", color="#060633", size=175, fontfamily=font1) +
+  draw_label(x=0.5, y=0.02, label =   "Twitter: @BlakeRobMills | Source: OpenStreetMap.org | GitHub: BlakeRMills", color="#060639", size=140, fontfamily=font1, fontface="bold") +
+  #draw_line(x=c(0, 0.18), y=c(0.02, 0.02), color="#060639", size=3.3) +
+  #draw_line(x=c(0.82, 1), y=c(0.02, 0.02), color="#060639", size=3.3) +
+  draw_label(x=0.15, y=0.908, label = "BERLIN", color="#060639", size=200, fontfamily=font2, fontface="bold") +
+  draw_label(x=0.34, y=0.908, label = "LONDON", color="#060639", size=200, fontfamily=font2, fontface="bold") +
+  draw_label(x=0.54, y=0.908, label = "MILAN", color="#060639", size=200, fontfamily=font2, fontface="bold") +
+  draw_label(x=0.73, y=0.908, label = "NEW YORK", color="#060639", size=200, fontfamily=font2, fontface="bold") +
+  draw_label(x=0.9, y=0.908, label = "PARIS", color="#060639", size=200, fontfamily=font2, fontface="bold") +
+  draw_label(x=0.042, y=0.83, label = "BIKE\nLANES", color=Colors[1], size=200, fontfamily=font2, fontface="bold", angle = 90, lineheight = 0.28) +
+  draw_label(x=0.042, y=0.65, label = "GREEN\nSPACE", color=Colors[2], size=200, fontfamily=font2, fontface="bold", angle = 90, lineheight = 0.28) +
+  draw_label(x=0.042, y=0.48, label = "PEDESTRIAN\nSTREETS", color=Colors[3], size=200, fontfamily=font2, fontface="bold", angle = 90, lineheight = 0.28) +
+  draw_label(x=0.042, y=0.3, label = "TRAINS &\nSUBWAYS", color=Colors[4], size=200, fontfamily=font2, fontface="bold", angle = 90, lineheight = 0.28) +
+  draw_label(x=0.042, y=0.11, label = "BODIES\nOF WATER", color=Colors[5], size=200, fontfamily=font2, fontface="bold", angle = 90, lineheight = 0.28) 
+
+
+ggsave("~/Desktop/Day 9 (Monochrome).png", height = 40, width = 35)
---
+++ 2021/Day 12 - Population/Day 12 (Population).R
@@ -0,0 +1,112 @@
+library(geojsonio)
+library(sf)
+library(tidyverse)
+library(readr)
+library(plyr)
+library(showtext)
+library(cowplot)
+
+# Aes
+showtext_auto()
+font_add_google("Josefin Sans")
+font_add_google("Cabin")
+font_add_google("Barlow Condensed")
+font1 <- "Josefin Sans"
+font2 <- "Barlow Condensed"
+font3 <- "Cabin"
+
+nycwards <- geojson_sf("~/Desktop/Day 12 (Population)/nyc-wards.geojson") 
+census <- read_csv("~/Desktop/Day 12 (Population)/NYSCensus.csv")
+
+
+MontFix <- data.frame(id="1703 North", name="North", type="st:Ward", validSince=1703, validUntil=1785,
+                      Population=1138, CensusYear=1703, geometry=st_union((nycwards %>% filter(name %in% c("East", "Montgomery")))))
+
+nycWards <- nycwards %>%  
+  as.data.frame(.) %>% 
+  left_join(census, by= c("name", "validSince")) %>%
+  filter(id != "1703-1785-montgomery", id != "1703-1785-east", is.na(CensusYear)==FALSE)  %>%
+  rbind.fill(MontFix)
+
+x <- nycWards %>% group_by(validSince) %>%
+  dplyr::summarize(sum = sum(Population))
+
+y <- nycWards %>% left_join(x, by="validSince") %>% mutate(prop = Population/sum)
+  
+
+rm(MontFix, nycwards, census)
+
+plotter <- function(year){
+  ggplot() +
+    geom_sf(data=nycWards %>% filter(validSince==year), aes(geometry=geometry, fill=Population), color="#fdf7ee") +
+    scale_fill_stepsn(limits = c(0, 60000),
+                      breaks = c(0, 500, 1000, 2000, 5000, 10000, 20000, 30000, 40000, 50000, 60000),
+                      values = scales::rescale(c(0, 500, 1000, 2000, 5000, 10000, 20000, 30000, 40000, 50000, 60000)),
+                      colors=rev(c("#1a1b33", "#2c2d54", "#434475", "#646b8e", "#8692a7", "#87a0ac", "#88b6a7", "#6f9774", "#577842", "#3f5730", "#324727")))+
+    theme_void() +
+    theme(plot.background = element_rect(fill="#fdf7ee", color="#fdf7ee"),
+          panel.background = element_rect(fill="#fdf7ee", color="#fdf7ee"),
+          legend.position = "none") 
+}
+
+Y1808 <- ggplot() +
+  geom_sf(data=nycWards %>% filter(validSince==1808), aes(geometry=geometry, fill=Population), color="#fdf7ee") +
+  scale_fill_stepsn(limits = c(0, 60000),
+                    breaks = c(0, 500, 1000, 2000, 5000, 10000, 20000, 30000, 40000, 50000, 60000),
+                    labels=scales::comma,
+                    values = scales::rescale(c(0, 500, 1000, 2000, 5000, 10000, 20000, 30000, 40000, 50000, 60000)),
+                    colors=rev(c("#1a1b33", "#2c2d54", "#434475", "#646b8e", "#8692a7", "#88b6a7", "#6f9774", "#577842", "#3f5730", "#324727")))+
+  theme_void() +
+  theme(plot.background = element_rect(fill="#fdf7ee", color="#fdf7ee"),
+        panel.background = element_rect(fill="#fdf7ee", color="#fdf7ee"),
+        legend.position = c(0.5, 1.06),
+        legend.direction = "horizontal",
+        legend.key.width = unit(8.5, "cm"),
+        legend.key.height = unit(1.2, "cm"),
+        legend.title = element_blank(),
+        legend.text = element_blank())
+
+grid <- plot_grid(plotter(1703),
+          plotter(1789),
+          Y1808,
+          plotter(1831),
+          plotter(1853),
+          nrow=1) +
+  theme(plot.background = element_rect(fill="#fdf7ee", color="#fdf7ee"),
+        panel.background = element_rect(fill="#fdf7ee", color="#fdf7ee"),
+        plot.margin = margin(9, 1.25, 0, 1, "cm")) 
+
+ggdraw(grid) +
+  draw_label(label = "Twitter: @BlakeRobMills | Source: New York Public Library & New York State Census | GitHub: BlakeRMills", x=0.5, y=0.022, color="#324727", fontface="bold", size=75, fontfamily = font2) +
+  draw_label(label="Old New York Population Over Time by Ward", x=0.5, y=0.95, size=275, fontface="bold", fontfamily = font1, color="#324727") +
+  draw_line(x=c(0, 0.05), y=c(0.95, 0.95), size=3.5, color="#324727") +
+  draw_line(x=c(0.95, 1), y=c(0.95, 0.95), size=3.5, color="#324727") +
+  draw_label(label="From 1683 to 1938, New York organized itself into wards that served as electoral districts and other administrative functions. Maps show the population of wards\nover time using historical New York Census data from 1703 to 1855. Historical boundaries of wards are provided by the New York Public Library.", 
+             x=0.5, y=0.86, size=105, lineheight=0.3, fontfamily=font2, color="#3d5f3c") +
+  draw_label(label="1703", x=0.061, y=0.473, size=150, fontface="bold", angle=55, fontfamily = font3, color="#324727") +
+  draw_label(label="Population: 4,446", x=0.077, y=0.44, size=110, angle=55, fontfamily = font2, color="#324727") +
+  draw_label(label="1790", x=0.258, y=0.472, size=150, fontface="bold", angle=55, fontfamily = font3, color="#88b6a7") +
+  draw_label(label="Population: 32,328", x=0.271, y=0.44, size=110, angle=55, fontfamily = font2, color="#88b6a7") +
+  draw_label(label="1810", x=0.451, y=0.472, size=150, fontface="bold", angle=55, fontfamily = font3, color="#6f9774") +
+  draw_label(label="Population: 96,373", x=0.465, y=0.44, size=110, angle=55, fontfamily = font2, color="#6f9774") +
+  draw_label(label="1835", x=0.646, y=0.472, size=150, fontface="bold", angle=55, fontfamily = font3, color="#646b8e") +
+  draw_label(label="Population: 268,089", x=.659, y=0.44, size=110, angle=55, fontfamily = font2, color="#646b8e") +
+  draw_label(label="1855", x=0.841, y=0.472, size=150, fontface="bold", angle=55, fontfamily = font3, color="#8692a7") +
+  draw_label(label="Population: 629,810", x=0.853, y=0.44, size=110, angle=55, fontfamily = font2, color="#8692a7") +
+  draw_label(label="500", x=0.28, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="1,000", x=0.335, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="2,000", x=0.39, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="5,000", x=0.445, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="10,000", x=0.5, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="20,000", x=0.555, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="30,000", x=0.61, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="40,000", x=0.665, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="50,000", x=0.72, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="60,000", x=0.775, y=0.7, size=60, fontfamily = font3) +
+  draw_label(label="Ward Population", x=0.5, y=0.79, size=150, fontfamily = font1, fontface="bold", color="#1a1b33") +
+  draw_line(x=c(0.225, 0.405), y=c(0.785, 0.785), size=1.25, color="#1a1b33") +
+  draw_line(x=c(0.595, 0.775), y=c(0.785, 0.785), size=1.25, color="#1a1b33") 
+  
+
+
+ggsave("~/Desktop/Day 12 Population Map.png", height = 15, width = 30)
