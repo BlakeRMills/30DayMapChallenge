d918fb8e218430029f023015e1df0cc8c384fd7e
Blake Robert Mills <82824419+BlakeRMills@users.noreply.github.com>
2023-11-28 22:57:46 -0500

    Day 26- Minimal

--- 2023/Day 26 - Minimal/Code/Day 26 - Minimal.R
+++ 2023/Day 26 - Minimal/Code/Day 26 - Minimal.R
@@ -9,25 +9,52 @@ library(showtext)
 library(sysfonts)
 library(tidyverse)
 
+# Aes
+
+# Function
+`%notin%` <- Negate(`%in%`)
+
 # Data
-NYC <- geojson_sf("~/30DayMapChallenge/2023/Day 01 - Points/Data/Borough Boundaries.geojson")
 Man <-  geojson_sf("~/30DayMapChallenge/2023/Day 01 - Points/Data/Borough Boundaries.geojson") %>%
   filter(boro_name == "Manhattan") %>%
   st_cast("POLYGON") %>%
   mutate(Area = st_area(.)) %>%
   arrange(-Area) %>%
-  .[c(1,5), ] %>%
+  .[c(1, 5), ] %>%
   st_combine()
 
-x <- st_make_grid(Man, n=c(24, 50))
+Neigh <- geojson_sf("~/30DayMapChallenge/2023/Day 19 - 5 Minute Map/Data/NYC_Neighborhoods_BetaNYC.geojson") %>%
+  filter(borough == "Manhattan", 
+         neighborhood %notin% c("Randall's Island", "Roosevelt Island"))
 
-t <- st_as_sf(x)[Man, ]
+# Cleaning  
+Grid <- st_make_grid(Man, n=c(24, 50))
+
+Grid <- st_as_sf(Grid)[Man, ] %>%
+  mutate(id = row_number())
+
+main_neigh <- st_intersection(Neigh, Grid) %>% 
+  mutate(intersect_area = st_area(.)) %>%   # create new column with shape area
+  dplyr::select(id, intersect_area, neighborhood) %>%   # only select columns needed to merge
+  st_drop_geometry() %>%
+  group_by(id) %>%
+  slice_max(order_by = intersect_area) %>%
+  ungroup()
+
+grid_final <- left_join(Grid, main_neigh, by = "id") %>%
+  mutate(center = st_centroid(x),
+         image = case_when(neighborhood == "Central Park" ~ "~/Desktop/Drawn 8 Bit/Tree.PNG",
+                           neighborhood %in% c("Upper East Side", "Upper West Side") ~ "~/Desktop/Drawn 8 Bit/Brownstone_1.PNG",
+                           neighborhood %in% c("Harlem", "Lower East Side", "West Village") ~ "~/Desktop/Drawn 8 Bit/Brown3.PNG",
+                           neighborhood == "Chinatown" ~ "~/Desktop/Drawn 8 Bit/Lantern.PNG"
+                           ))
 
 
-ggplot() +
-  geom_sf(data=t) 
-  geom_sf(data=Man) +
-  geom_sf(data = x, fill="transparent", color="red") +
+m <- ggplot() +
+  geom_sf(data=grid_final, aes(fill=neighborhood, geometry=x), color="transparent") +
+  geom_image(data=grid_final, aes(geometry=center, image=image), stat="sf_coordinates", size =0.0183) +
+  theme_void() +
+  #theme(legend.position="none") +
   coord_sf()
 
 ggsave("~/30DayMapChallenge/2023/Day 26 - Minimal/Day 26 - Minimal.png", width=11, height=14)
